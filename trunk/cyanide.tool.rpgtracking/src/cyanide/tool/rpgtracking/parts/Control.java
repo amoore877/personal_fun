51c35b2fb6c53e37a5b9e6983bb4080382ca84c2
amoore877 <amoore877@a873568d-a793-46d3-b71a-2286bc230a97>
2016-05-22 21:07:25 +0000

    Very basic, Hello World, working e4 product

---
+++ trunk/cyanide.tool.rpgtracking/src/cyanide/tool/rpgtracking/parts/Control.java
@@ -0,0 +1,22 @@
+ 
+package cyanide.tool.rpgtracking.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class Control {
+	@Inject
+	public Control() {
+		
+	}
+	
+	@PostConstruct
+	public void createControls(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
