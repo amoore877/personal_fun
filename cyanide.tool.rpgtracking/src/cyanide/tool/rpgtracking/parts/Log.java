322d5b7f86added92b9e81b756f4112a5e44e11c
amoore877 <amoore877@gmail.com>
2016-05-22 21:07:25 +0000

    Very basic, Hello World, working e4 product

---
+++ cyanide.tool.rpgtracking/src/cyanide/tool/rpgtracking/parts/Log.java
@@ -0,0 +1,22 @@
+ 
+package cyanide.tool.rpgtracking.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class Log {
+	@Inject
+	public Log() {
+		
+	}
+	
+	@PostConstruct
+	public void createControls(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
---
+++ cyanide.tool.rpgtracking/src/cyanide/tool/rpgtracking/parts/View.java
@@ -0,0 +1,22 @@
+ 
+package cyanide.tool.rpgtracking.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class View {
+	@Inject
+	public View() {
+		
+	}
+	
+	@PostConstruct
+	public void createControls(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
