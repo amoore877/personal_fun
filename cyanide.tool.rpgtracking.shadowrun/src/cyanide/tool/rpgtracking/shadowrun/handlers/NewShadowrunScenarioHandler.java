25ca2e97ed4eb4e5240dea6d3236024cfa3a6edb
amoore877 <amoore877@gmail.com>
2016-05-23 00:13:50 +0000

    e4 is not mature enough yet to allow for readable, maintainable implementation. Put away RPG Tracking product idea for now.

---
+++ cyanide.tool.rpgtracking.shadowrun/src/cyanide/tool/rpgtracking/shadowrun/handlers/NewShadowrunScenarioHandler.java
@@ -0,0 +1,13 @@
+ 
+package cyanide.tool.rpgtracking.shadowrun.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.swt.widgets.Shell;
+
+public class NewShadowrunScenarioHandler {
+	@Execute
+	public void execute(Shell shell) {
+		
+	}
+		
+}
