6a99294e2a760f61100bdb678d2c5d005fd250cd
amoore877 <amoore877@a873568d-a793-46d3-b71a-2286bc230a97>
2016-06-05 00:45:22 +0000

    Slight reorg. Little bit of logic added.

---
+++ trunk/recipebook/src/recipebook/gui/RecipeBookGUIUtil.java
@@ -0,0 +1,25 @@
+package recipebook.gui;
+
+/**
+ * Utility class for GUI components of Recipe Book programs.
+ * 
+ * @author Andrew
+ *
+ */
+public class RecipeBookGUIUtil {
+
+	/**
+	 * Extension for recipe books.
+	 */
+	public static final String BOOK_EXT = ".rbook";
+
+	/**
+	 * Height for buttons.
+	 */
+	public static final int BUTTON_HEIGHT = 23;
+
+	/**
+	 * Width for buttons.
+	 */
+	public static final int BUTTON_WIDTH = 121;
+}
